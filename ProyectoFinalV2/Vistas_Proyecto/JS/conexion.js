7ef7b246ff855b8ac6e09849f59ec73cf2a599a7
IkeRoma <adrikerrm00@gmail.com>
2025-12-04 20:30:16 -0600

    nuevaBasePrueba1Deploy

---
+++ ProyectoFinalV2/Vistas_Proyecto/JS/conexion.js
@@ -0,0 +1,18 @@
+let mysql = require("mysql2");
+
+let conexion = mysql.createConnection({
+    host: "localhost",
+    database: "prueba2",
+    user: "root",
+    password: "5485Roma"
+});
+
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    } else {
+        console.log("Conexion exitosa");
+    }
+});
+
+conexion.end();
